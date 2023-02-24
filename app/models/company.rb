61a681e973c8a9e50c77cdf26c41032a2880cb73
Volodymyr Shafran <shafran.valdemarovich@gmail.com>
2023-02-24 12:13:35 +0200

    make name in upcase

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,7 @@ class Company < ApplicationRecord
   has_many :products, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
+  validates :endpoint, presence: true
+
+  before_validation { name.upcase! }
 end
